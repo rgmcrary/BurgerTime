0ec885b044fd5e30ae6cec8fee1dbc87b36d8a8b
Rich McRary <rgmcrary@gmail.com>
2017-09-29 11:15:42 -0400

    added values

--- config/orm.js
+++ config/orm.js
@@ -12,14 +12,14 @@ var orm = {
   insertOne: function(table, whatToInsert, values) {
     var queryString = "INSERT INTO ?? (??, ??, ??) VALUES (??, ??, ??)";
     console.log(queryString);
-    connection.query(queryString, [table, whatToInsert], function(err, result) {
+    connection.query(queryString, [table, whatToInsert, values], function(err, result) {
       console.log(result);
     });
   },
   updateOne: function(table, whatToUpdate, values) {
     var queryString = "UPDATE ?? SET (??, ??, ??) WHERE (??, ??, ??)";
 
-    connection.query(queryString, [table, whatToUpdate, tableOneCol], function(err, result) {
+    connection.query(queryString, [table, whatToUpdate, values], function(err, result) {
       console.log(result);
     });
   }
