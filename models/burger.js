008ba0ca153497393fce66b1d9825b84459138ec
Rich McRary <rgmcrary@gmail.com>
2017-09-29 11:10:37 -0400

    Created functions

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+var orm = require("./config/orm.js");
+
+// Find all burgers in the burgers table.
+orm.selectAll("burgers");
+
+// Insert new burger into the burgers table.
+orm.insertOne("burgers", (burger, devoured, date), ("", FALSE, CURRENT_TIMESTAMP));
+
+// Updates the devoured field in the burgers table.
+orm.updateOne("burgers", (burger, devoured, date), ("", TRUE, CURRENT_TIMESTAMP));
