50e9f8cb743a426e54f9c87501d51ff2c040d436
Rich McRary <rgmcrary@gmail.com>
2017-09-29 09:48:39 -0400

    Wrote insert queries to populate the `burgers` table

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger, devoured, date)
+VALUES ("bacon cheeseburger", TRUE, 2017-08-12), 
+        ("mushroom swiss burger", TRUE, 2017-09-25), 
+        ("triple cheeseburger", FALSE, 2017-09-26);
+
+SELECT * FROM burgers;
