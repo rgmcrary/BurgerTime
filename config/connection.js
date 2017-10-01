0f559f591170ad8a0223ee21f0a34ed5c3ca4029
Rich McRary <rgmcrary@gmail.com>
2017-10-01 15:00:29 -0400

    Added connection for MySQL for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
