0049e61aeec503c8b5c0ca50c51f95f510178404
Rich McRary <rgmcrary@gmail.com>
2017-09-30 20:06:40 -0400

    Updated routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,22 @@
-var express = require('express');
+var express = require("express");
+
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
+router.get("/api/burgers", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.json(hbsObject);
+  });
+});
+
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,24 +26,15 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burger", function(req, res) {
-  burger.insertOne([
-    "burger", "devoured"
-  ], [
-    req.body.burger, req.body.devoured
-  ], function(result) {
+  burger.insertOne(
+    req.body.burger, function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burger/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
+  burger.updateOne(req.params.id, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
