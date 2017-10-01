08a20c981292aded7454a6a8c9f1ae0dfca41452
Rich McRary <rgmcrary@gmail.com>
2017-10-01 09:44:45 -0400

    added numbering helper function to engine

--- server.js
+++ server.js
@@ -6,19 +6,38 @@ var port = 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+var exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+    helpers: {
+      // Function to do basic mathematical operation in handlebar
+      math: function(lvalue, operator, rvalue) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+        return {
+          '+': lvalue + rvalue,
+          '-': lvalue - rvalue,
+          '*': lvalue * rvalue,
+          '/': lvalue / rvalue,
+          '%': lvalue % rvalue
+        }[operator];
+      }
+    }
+  })
+);
+app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require('./controllers/burgers_controller.js');
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(port);
